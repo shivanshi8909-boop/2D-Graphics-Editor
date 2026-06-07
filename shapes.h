fa27e4f72485f06aa6e910662a4969c740f0f585
Shivanshi <shivanshi8909@gmail.com>
2026-06-07 13:58:30 +0530

    Completed graphics editor functionality

--- shapes.h
+++ shapes.h
@@ -1,43 +1,37 @@
 #ifndef SHAPES_H
 #define SHAPES_H
 
-#define ROWS 25
-#define COLS 80
+#define ROWS 50
+#define COLS 150
 #define MAX_SHAPES 100
 
 #define RECTANGLE 1
-#define LINE      2
-#define TRIANGLE  3
-#define CIRCLE    4
+#define LINE 2
+#define TRIANGLE 3
+#define CIRCLE 4
 
-typedef struct
-{
-    int id;
-    int type;
+typedef struct {
+  int id;
+  int type;
 
-    int x1, y1;
-    int x2, y2;
-    int x3, y3;
+  int x1, y1;
+  int x2, y2;
+  int x3, y3;
 
-    int width;
-    int height;
-    int radius;
+  int width;
+  int height;
+  int radius;
 
 } Shape;
 
-void drawRectangle(char canvas[ROWS][COLS],int x,int y,int width,int height);
+void drawRectangle(char canvas[ROWS][COLS], int x, int y, int width,
+                   int height);
 
-void drawLine(char canvas[ROWS][COLS],int x1, int y1, 
-                                      int x2,int y2);
+void drawLine(char canvas[ROWS][COLS], int x1, int y1, int x2, int y2);
 
-void drawTriangle(char canvas[ROWS][COLS],
-                  int x1, int y1,
-                  int x2, int y2,
+void drawTriangle(char canvas[ROWS][COLS], int x1, int y1, int x2, int y2,
                   int x3, int y3);
 
-void drawCircle(char canvas[ROWS][COLS],
-                int centerX,
-                int centerY,
-                int radius);
+void drawCircle(char canvas[ROWS][COLS], int centerX, int centerY, int radius);
 
 #endif
